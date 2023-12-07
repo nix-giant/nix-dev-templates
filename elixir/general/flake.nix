a93bb43cff8a76448e45194325f95c32f835c525
c4710n <c4710n@users.noreply.github.com>
2023-12-07 10:42:36 +0800

    feat(elixir/general): don't limit hex to current project anymore

--- elixir/general/flake.nix
+++ elixir/general/flake.nix
@@ -2,7 +2,7 @@
   description = "Elixir development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -37,15 +37,11 @@
           shellHook = ''
             # limit mix to current project
             mkdir -p .nix-mix
-            mkdir -p .nix-hex
             export MIX_HOME=$PWD/.nix-mix
-            export HEX_HOME=$PWD/.nix-hex
-            export ERL_LIBS=$HEX_HOME/lib/erlang/lib
 
             # rewire executables
             export PATH=$MIX_HOME/bin:$PATH
             export PATH=$MIX_HOME/escripts:$PATH
-            export PATH=$HEX_HOME/bin:$PATH
 
             # limit history to current project
             export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang-history\"'"
